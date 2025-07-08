2af288f118b7d24ef6fc3195b0b92271951bd54a
Shiva Kumar <shiva@example.com>
2025-07-08 16:12:38 +0530

    testapp.py

--- testapp.py
+++ testapp.py
@@ -31,5 +31,8 @@ def test_api():
             "error": str(e)
         }), 500
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
